92c0c1dc9329955217ed4da21032ee684dbb7142
deepakselva23 <deepakselva2304@gmail.com>
2024-04-12 21:34:52 +0530

    added profile-bio

--- twitter/form.py
+++ twitter/form.py
@@ -10,10 +10,16 @@ from django.contrib.auth.models import User
 class ProfilePicForm(forms.ModelForm):
 
     profile_image = forms.ImageField(label="Profile Picture")
+
+    profile_bio = forms.CharField(label="", widget=forms.widgets.Textarea(attrs={"placeholder":"Profile Bio","class":"form-control"}),)
+    homepage_link = forms.CharField(label="", widget=forms.widgets.TextInput(attrs={"placeholder":"Home page Link","class":"form-control"}),)
+    instagram_link = forms.CharField(label="", widget=forms.widgets.TextInput(attrs={"placeholder":"Instagram Link","class":"form-control"}),)
+    facebook_link = forms.CharField(label="", widget=forms.widgets.TextInput(attrs={"placeholder":"FaceBook Link","class":"form-control"}),)
+    linkedin_link = forms.CharField(label="", widget=forms.widgets.TextInput(attrs={"placeholder":"Linkin Link","class":"form-control"}),)
     
     class Meta:
         model = Profile
-        fields = ('profile_image', )
+        fields = ('profile_image', 'profile_bio', 'homepage_link', 'instagram_link', 'facebook_link', 'linkedin_link' )
 
 
 class MeepForm(forms.ModelForm):
